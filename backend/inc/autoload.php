b71f553594541f59796e8d068a93f501e89f5abf
Michael Arawole <39175160+michael-arawole@users.noreply.github.com>
2022-08-01 08:18:37 +0100

    Update autoload.php
    Removed copyright header

--- backend/inc/autoload.php
+++ backend/inc/autoload.php
@@ -5,18 +5,14 @@
 // | @author_email  : logadscripts@gmail.com
 // | @date          : 10 Jul, 2022 11:04 AM
 // +------------------------------------------------------------------------+
-// | Copyright (c) 2022 Logad Networks. All rights reserved.
+// | 2022 Logad Networks
 // +------------------------------------------------------------------------+
 
 // +----------------------------+
 // | AutoLoader
 // +----------------------------+
 
-//require dirname(__FILE__).'/functions.php';
-//$app = new app();
-
 spl_autoload_register('synoteAutoLoader');
-
 function synoteAutoLoader($className) {
     $path = dirname(__FILE__, 2) . "/classes/";
     $extension = ".class.php";
